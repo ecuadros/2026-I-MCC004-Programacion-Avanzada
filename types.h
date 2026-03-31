6b496fbab03e65139021ea27066239bf85eaa139
gretadive <gretadiaz01@gmail.com>
2026-03-30 22:03:17 -0500

    eliminacion types.h
    eliminacion

--- types.h
+++ types.h
@@ -1,19 +1,14 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
+
 #include <cstdint>
 
-// 3 Castro Chaupis, Armando Cristhian <armandocristhiancastrochaupis@gmail.com>,
-using T3F = float;
+using T1 = int32_t;
+using U1 = uint32_t;
 
-
-using T1  = int32_t;
 using T5 = double;
 using T16 = double;
 
-using U1  = uint32_t;
-
 using T_ABS = double;
 
-#endif // __TYPES_H__
-
-
+#endif
