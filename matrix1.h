1f1551879b30a6eded72a82652fa5e1398950ae3
Jorge Miranda <mirandazaratejorge@gmail.com>
2026-06-01 11:50:27 -0500

    Implement operator+, operator- and operator*

--- matrix1.h
+++ matrix1.h
@@ -27,6 +27,12 @@ class Matrix1 {
         void ApplyFunctionToAll(Func func, Args&& ...args);
         ostream &Print(ostream &os);
         void Destroy();
+
+        // Operator overloading
+        Matrix1 operator+(Matrix1& m);
+        Matrix1 operator-(Matrix1& m);
+        Matrix1 operator*(Matrix1& m);
+        Matrix1 operator*(T value);
 };
 
 template <typename T>
@@ -40,6 +46,8 @@ void Matrix1<T>::Create()
 // Move constructor
 template <typename T>
 Matrix1<T>::Matrix1(Matrix1 &&other) {
+    static TI mov_cnt;
+    cout << "Move constructor called : " << ++mov_cnt << endl;
     m_pMat = exchange(other.m_pMat, nullptr);
     m_rows = exchange(other.m_rows, 0);
     m_cols = exchange(other.m_cols, 0);
@@ -108,6 +116,71 @@ template<typename T>
 ostream &operator<<(ostream &os, Matrix1<T> &matrix){
     matrix.ApplyFunctionToAll(Print4<T>, os);
     return os;
+}
+
+template <typename T>
+Matrix1<T> Matrix1<T>::operator+(Matrix1<T>& m){
+    assert(this->m_rows == m.m_rows && this->m_cols == m.m_cols);
+    Matrix1<T> res;
+    res.m_rows = m.m_rows;
+    res.m_cols = m.m_cols;
+    res.Create();
+
+    for(size_t i = 0; i < res.m_rows; ++i){
+        for(size_t j = 0; j < res.m_cols; ++j){
+            res.m_pMat[i][j] = this->m_pMat[i][j] + m.m_pMat[i][j];
+        }
+    }
+    return res;
+}
+
+template <typename T>
+Matrix1<T> Matrix1<T>::operator-(Matrix1<T>& m){
+    assert(this->m_rows == m.m_rows && this->m_cols == m.m_cols);
+    Matrix1<T> res;
+    res.m_rows = m.m_rows;
+    res.m_cols = m.m_cols;
+    res.Create();
+
+    for(size_t i = 0; i < res.m_rows; ++i){
+        for(size_t j = 0; j < res.m_cols; ++j){
+            res.m_pMat[i][j] = this->m_pMat[i][j] - m.m_pMat[i][j];
+        }
+    }
+    return res;
+}
+
+template <typename T>
+Matrix1<T> Matrix1<T>::operator*(Matrix1<T>& m){
+    assert(this->m_cols == m.m_rows);
+    Matrix1<T> res;
+    res.m_rows = this->m_rows;
+    res.m_cols = m.m_cols;
+    res.Create();
+
+    for(size_t i = 0; i < res.m_rows; ++i){
+        for(size_t j = 0; j < res.m_cols; ++j){
+            res.m_pMat[i][j] = 0;
+            for(size_t k = 0; k < this->m_cols; ++k){
+                res.m_pMat[i][j] += this->m_pMat[i][k]*m.m_pMat[k][j];
+            }
+        }
+    }
+    return res;
+}
+
+template <typename T>
+Matrix1<T> Matrix1<T>::operator*(T value){
+    Matrix1<T> res;
+    res.m_rows = this->m_rows;
+    res.m_cols = this->m_cols;
+
+    for(size_t i = 0; i < res.m_rows; ++i){
+        for(size_t j = 0; j < res.m_cols; ++j){
+            res.m_pMat[i][j] = this->m_pMat[i][j]*value;
+        }
+    }
+    return res;
 }
 
 #endif // __MATRIX_H__
