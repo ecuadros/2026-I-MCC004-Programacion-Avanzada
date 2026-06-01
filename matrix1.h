4faee921711477a9388dac7c8a0d0810cae11d22
quispedavid987 <quispedavid987@gmail.com>
2026-06-01 15:24:10 -0500

    Implementig matrix

--- matrix1.h
+++ matrix1.h
@@ -1,8 +1,10 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
-#include <__nullptr>
+
 #include <functional>
 #include <iostream>
+#include <cassert>
+#include "types.h"
 
 using namespace std;
 
@@ -15,8 +17,8 @@ class Matrix1 {
         T      **m_pMat = nullptr;
         size_t   m_rows = 0, m_cols = 0;
     public:
-        Matrix1()      { }
-        ~Matrix1()     { Destroy(); }
+        Matrix1(size_t rows=0, size_t cols=0) : m_rows(rows), m_cols(cols) { } // constructor
+        ~Matrix1()     { Destroy(); } // destructor
         void     Create();
         istream &Read(istream &is);
         template <typename Func, typename... Args>
@@ -28,11 +30,62 @@ class Matrix1 {
 template <typename T>
 void Matrix1<T>::Create()
 {   assert(m_rows > 0 && m_cols > 0);
-    m_pMat = new T *[m_rows];
+    m_pMat = new T *[m_rows]; // creando array de punteros (filas)
     for(size_t i = 0 ; i < m_rows ; ++i)
-        m_pMat[i] = new T[m_cols];
-}
+        m_pMat[i] = new T[m_cols]; // creando columnas
+};
 
+template <typename T>
+istream &Matrix1<T>::Read(istream &is){
+    Destroy();
+    is >> m_rows >> m_cols;
+    Create();
+    for(size_t i = 0 ; i < m_rows ; ++i)
+        for(size_t j = 0 ; j < m_cols ; ++j)
+            is >> m_pMat[i][j];
+    return is;
+};
+
+template <typename T>
+template <typename Func, typename... Args>
+void Matrix1<T>::ApplyFunctionToAll(Func func, Args&& ...args){
+    for(size_t i = 0 ; i < m_rows ; ++i)
+        for(size_t j = 0 ; j < m_cols ; ++j)
+            func(m_pMat[i][j], forward<Args>(args)...);
+};
+
+template <typename T>
+ostream &Matrix1<T>::Print(ostream &os){
+    for(size_t i = 0 ; i < m_rows ; ++i){
+        for(size_t j = 0 ; j < m_cols ; ++j)
+            os << m_pMat[i][j] << " ";
+        os << endl;
+    }
+    return os;
+};
+
+template <typename T>
+void Matrix1<T>::Destroy(){
+    if(m_pMat == nullptr) return;
+    for(size_t i = 0 ; i < m_rows ; ++i)
+        delete[] m_pMat[i];
+    delete[] m_pMat;
+    m_pMat = nullptr;
+    m_rows = m_cols = 0;
+};
+
+template <typename T>
+istream &operator>>(istream &is, Matrix1<T> &matrix){
+    return matrix.Read(is);
+};
+
+template <typename T>
+ostream &operator<<(ostream &os, Matrix1<T> &matrix){
+    matrix.Print(os);
+    return os;
+};
+
+void DemoMatrix();
 
 
 #endif // __MATRIX_H__
