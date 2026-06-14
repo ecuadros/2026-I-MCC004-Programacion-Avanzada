a9926ad1b492eaefeb28971a1301f33757f9080e
Greta Diaz <gretadiaz01@gmail.com>
2026-06-14 00:45:33 -0500

    Tarea: 12-matriz

--- main.cpp
+++ main.cpp
@@ -7,9 +7,23 @@
 #include "polimorfismo.h"
 #include "Pointers.h"
 #include "BitSigno.h"
+#include "matrix1.h"
 
 using namespace std;
 
+void DemoMatrix1()
+{   cout<<"Demo Matrix1" << endl;
+    Matrix1<int> m;
+    cout<<"Datos de la matriz:" << endl;
+    cin>>m;
+    cout<<endl;
+    cout<<"Matriz<<:" << endl;
+    cout<<m;
+    cout<<endl;
+    cout<<"El resultado sería:"<<endl;
+    m.ApplyFunctionToAll(Print4<int>,cout);
+    cout<<endl;
+}
 // Ernesto Cuadros
 // g++ -std=c++2b main.cpp util.cpp -o main
 int main() {
@@ -21,7 +35,9 @@ int main() {
     // DemoPointersVector1();
     // DemoPointersVector2();
     // DemoPointersVector3();
-    DemoPointersVector4();
-    DemoPointersVector5();
+    // DemoPointersVector4();
+    // DemoPointersVector5();
+    
+    DemoMatrix1();
     return 0;
 }
--- matrix1.h
+++ matrix1.h
@@ -1,8 +1,9 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
-#include <__nullptr>
-#include <functional>
 #include <iostream>
+#include <functional>
+#include <cassert>
+#include <cstddef>
 
 using namespace std;
 
@@ -33,6 +34,59 @@ void Matrix1<T>::Create()
         m_pMat[i] = new T[m_cols];
 }
 
+// parte para completar
+template <typename T>
+istream &Matrix1<T>::Read(istream &is)
+{ Destroy();
+    is >> m_rows >> m_cols;
+    Create();
+    {for(size_t i = 0 ; i < m_rows ; ++i)
+        for(size_t j = 0 ; j < m_cols ; ++j)
+        is >> m_pMat[i][j];
+    }
+    return is;
+}
 
+template <typename T>
+template <typename Func, typename... Args>
+void Matrix1<T>::ApplyFunctionToAll(Func func, Args&& ...args)
+{for(size_t i = 0 ; i < m_rows ; ++i)
+    {for(size_t j = 0 ; j < m_cols ; ++j)
+        {func(m_pMat[i][j], args...);}
+    }
+}
+
+template <typename T>
+ostream &Matrix1<T>::Print(ostream &os)
+{
+    for(size_t i = 0 ; i < m_rows ; ++i)
+    {
+        for(size_t j = 0 ; j < m_cols ; ++j)
+        {os << m_pMat[i][j] << " ";}
+        os << endl;
+    }
+
+    return os;
+}
+
+template <typename T>
+void Matrix1<T>::Destroy()
+{if(m_pMat != nullptr)
+    {for(size_t i=0;i<m_rows;++i)
+        {delete[] m_pMat[i];}
+        delete[] m_pMat;
+        m_pMat = nullptr;
+    }
+    m_rows = 0;
+    m_cols = 0;
+}
+
+template <typename T>
+istream &operator>>(istream &is, Matrix1<T> &mat)
+{return mat.Read(is);}
+
+template <typename T>
+ostream &operator<<(ostream &os, Matrix1<T> &mat)
+{return mat.Print(os);}
 
 #endif // __MATRIX_H__
