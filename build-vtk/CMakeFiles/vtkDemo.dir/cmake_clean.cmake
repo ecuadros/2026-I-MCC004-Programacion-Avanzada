e67d9e44289318921a822778f4bd94cb6bfaf037
susansm <ssuarez@givisasoft.com>
2026-07-14 00:14:38 -0500

    12-Matrix, Matrix y VTK

---
+++ build-vtk/CMakeFiles/vtkDemo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vtkDemo.dir/vtkDemo.cpp.o"
+  "CMakeFiles/vtkDemo.dir/vtkDemo.cpp.o.d"
+  "vtkDemo"
+  "vtkDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vtkDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
