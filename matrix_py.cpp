aeb4d247fbe3a6a083af95deeb786cbeffb7f1db
Greta Diaz <gretadiaz01@gmail.com>
2026-07-13 19:52:49 -0500

    Trabajo 16: plantilla para Matrix int y float en Python

--- matrix_py.cpp
+++ matrix_py.cpp
@@ -1,26 +1,46 @@
 #include <pybind11/pybind11.h>
 #include <sstream>
+#include <string>
+#include <stdexcept>
 #include "matrix1.h"
 
 namespace py = pybind11;
+using namespace std;
 
-PYBIND11_MODULE(matrix_py, m)
+template <typename T>
+void RegistrarMatrix(py::module_ &m, const char *nombre)
 {
-    py::class_<Matrix1<int>>(m, "Matrix")
+    py::class_<Matrix1<T>>(m, nombre)
         .def(py::init<size_t, size_t>())
 
-        .def("set", &Matrix1<int>::Set)
-        .def("get", &Matrix1<int>::Get)
-        .def("rows", &Matrix1<int>::Rows)
-        .def("cols", &Matrix1<int>::Cols)
+        .def("set", &Matrix1<T>::Set)
+        .def("get", &Matrix1<T>::Get)
+        .def("rows", &Matrix1<T>::Rows)
+        .def("cols", &Matrix1<T>::Cols)
 
-        .def("__mul__", [](const Matrix1<int> &a, const Matrix1<int> &b) {
+        .def("__mul__", [](const Matrix1<T> &a, const Matrix1<T> &b) {
             return a * b;
         })
 
-        .def("__str__", [](Matrix1<int> &mat) {
-            std::ostringstream os;
+        .def("__str__", [](Matrix1<T> &mat) {
+            ostringstream os;
             os << mat;
             return os.str();
         });
+}
+
+PYBIND11_MODULE(matrix_py, m)
+{
+    RegistrarMatrix<int>(m, "MatrixInt");
+    RegistrarMatrix<float>(m, "MatrixFloat");
+
+    m.def("crear_matriz", [](const string &tipo, size_t filas, size_t columnas) -> py::object {
+        if(tipo == "int")
+            return py::cast(Matrix1<int>(filas, columnas));
+
+        if(tipo == "float")
+            return py::cast(Matrix1<float>(filas, columnas));
+
+        throw invalid_argument("Tipo no valido. Use int o float.");
+    });
 }
--- test_matrix.py
+++ test_matrix.py
@@ -1,11 +1,31 @@
-from matrix_py import Matrix
+from matrix_py import MatrixInt, MatrixFloat, crear_matriz
 
-m = Matrix(2, 2)
+print("Matriz de enteros:")
 
+m = MatrixInt(2, 2)
 m.set(0, 0, 5)
 m.set(0, 1, 6)
 m.set(1, 0, 7)
 m.set(1, 1, 8)
 
-print("Matriz m:")
 print(m)
+
+print("Matriz de flotantes:")
+
+f = MatrixFloat(2, 2)
+f.set(0, 0, 1.5)
+f.set(0, 1, 2.5)
+f.set(1, 0, 3.5)
+f.set(1, 1, 4.5)
+
+print(f)
+
+print("Matriz creada con parametro tipo:")
+
+x = crear_matriz("float", 2, 2)
+x.set(0, 0, 10.5)
+x.set(0, 1, 20.5)
+x.set(1, 0, 30.5)
+x.set(1, 1, 40.5)
+
+print(x)
