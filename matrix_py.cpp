544f4e100d23d332bbefa6d35af968545b5b42b2
Greta Diaz <gretadiaz01@gmail.com>
2026-07-27 16:17:58 -0500

    Tarea 14: completar binding Python con plantilla

--- matrix_py.cpp
+++ matrix_py.cpp
@@ -1,26 +1,119 @@
 #include <pybind11/pybind11.h>
 #include <sstream>
+#include <stdexcept>
 #include "matrix1.h"
+#include "types.h"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(matrix_py, m)
+template <typename T>
+class MatrixRow
 {
-    py::class_<Matrix1<int>>(m, "Matrix")
+    private:
+        Matrix1<T> *matrix;
+        size_t row;
+
+    public:
+        MatrixRow(Matrix1<T> &m, size_t r)
+        {
+            matrix = &m;
+            row = r;
+        }
+
+        T get(size_t col) const
+        {
+            return matrix->Get(row, col);
+        }
+
+        void set(size_t col, T value)
+        {
+            matrix->Set(row, col, value);
+        }
+};
+
+template <typename T>
+void BindMatrix(py::module_ &m, const char *matrix_name, const char *row_name)
+{
+    using MatrixType = Matrix1<T>;
+    using RowType = MatrixRow<T>;
+
+    py::class_<RowType>(m, row_name)
+        .def("__getitem__", &RowType::get)
+        .def("__setitem__", &RowType::set);
+
+    py::class_<MatrixType>(m, matrix_name)
         .def(py::init<size_t, size_t>())
 
-        .def("set", &Matrix1<int>::Set)
-        .def("get", &Matrix1<int>::Get)
-        .def("rows", &Matrix1<int>::Rows)
-        .def("cols", &Matrix1<int>::Cols)
+        .def("set", &MatrixType::Set)
+        .def("get", &MatrixType::Get)
+        .def("rows", &MatrixType::Rows)
+        .def("cols", &MatrixType::Cols)
 
-        .def("__mul__", [](const Matrix1<int> &a, const Matrix1<int> &b) {
+        .def("__getitem__", [](MatrixType &mat, size_t row) {
+            return RowType(mat, row);
+        })
+
+        .def("__mul__", [](const MatrixType &a, const MatrixType &b) {
             return a * b;
         })
 
-        .def("__str__", [](Matrix1<int> &mat) {
+        .def("__mul__", [](const MatrixType &a, T value) {
+            return a * value;
+        })
+
+        .def("__rmul__", [](const MatrixType &a, T value) {
+            return a * value;
+        })
+
+        .def("__add__", [](const MatrixType &a, const MatrixType &b) {
+            return a + b;
+        })
+
+        .def("__sub__", [](const MatrixType &a, const MatrixType &b) {
+            return a - b;
+        })
+
+        .def("transpose", [](const MatrixType &mat) {
+            MatrixType result(mat.Cols(), mat.Rows());
+
+            for(size_t i = 0; i < mat.Rows(); ++i)
+                for(size_t j = 0; j < mat.Cols(); ++j)
+                    result.Set(j, i, mat.Get(i, j));
+
+            return result;
+        })
+
+        .def("trace", [](const MatrixType &mat) {
+            if(mat.Rows() != mat.Cols())
+                throw std::invalid_argument("La matriz debe ser cuadrada.");
+
+            T suma = T();
+
+            for(size_t i = 0; i < mat.Rows(); ++i)
+                suma = suma + mat.Get(i, i);
+
+            return suma;
+        })
+
+        .def("add_value", [](const MatrixType &mat, T value) {
+            MatrixType result(mat.Rows(), mat.Cols());
+
+            for(size_t i = 0; i < mat.Rows(); ++i)
+                for(size_t j = 0; j < mat.Cols(); ++j)
+                    result.Set(i, j, mat.Get(i, j) + value);
+
+            return result;
+        })
+
+        .def("__str__", [](MatrixType &mat) {
             std::ostringstream os;
             os << mat;
             return os.str();
         });
+}
+
+PYBIND11_MODULE(matrix_py, m)
+{
+    BindMatrix<TI>(m, "Matrix", "MatrixRow");
+    BindMatrix<T3F>(m, "MatrixDecimal", "MatrixDecimalRow");
 }
--- test_matrix.py
+++ test_matrix.py
@@ -1,11 +1,62 @@
-from matrix_py import Matrix
+from matrix_py import Matrix, MatrixDecimal
 
-m = Matrix(2, 2)
+print("=== Demo 1: hacer posible m[3][2] = 8 ===")
 
-m.set(0, 0, 5)
-m.set(0, 1, 6)
-m.set(1, 0, 7)
-m.set(1, 1, 8)
+m = Matrix(4, 3)
+m[3][2] = 8
 
-print("Matriz m:")
 print(m)
+
+print("=== Demo 2: m1 = m2 * m3 ===")
+
+m2 = Matrix(2, 3)
+m3 = Matrix(3, 2)
+
+valores_m2 = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+valores_m3 = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+]
+
+for i in range(2):
+    for j in range(3):
+        m2[i][j] = valores_m2[i][j]
+
+for i in range(3):
+    for j in range(2):
+        m3[i][j] = valores_m3[i][j]
+
+m1 = m2 * m3
+
+print(m1)
+
+print("=== Extra #1: transpuesta ===")
+print(m2.transpose())
+
+print("=== Extra #2: traza ===")
+
+cuadrada = Matrix(2, 2)
+cuadrada[0][0] = 1
+cuadrada[0][1] = 2
+cuadrada[1][0] = 3
+cuadrada[1][1] = 4
+
+print(cuadrada.trace())
+
+print("=== Extra #3: sumar un valor a toda la matriz ===")
+print(cuadrada.add_value(10))
+
+print("=== Demo con matriz decimal ===")
+
+f = MatrixDecimal(2, 2)
+f[0][0] = 1.5
+f[0][1] = 2.5
+f[1][0] = 3.5
+f[1][1] = 4.5
+
+print(f)
